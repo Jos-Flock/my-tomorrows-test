860c1420aab7001431abe10b8631f40075feaa52
Jos Verhaar <jos.verhaar@flock.community>
2023-09-13 09:42:50 +0200

    refactor: rename study component to study-card component

--- src/components/study-card/study-card.component.ts
+++ src/components/study-card/study-card.component.ts
@@ -2,11 +2,11 @@ import {Component, Input} from '@angular/core';
 import {Study} from "../../models/study";
 
 @Component({
-  selector: 'app-study',
-  templateUrl: './study.component.html',
-  styleUrls: ['./study.component.scss']
+  selector: 'app-study-card',
+  templateUrl: './study-card.component.html',
+  styleUrls: ['./study-card.component.scss']
 })
-export class StudyComponent {
+export class StudyCardComponent {
 
   @Input() study: Study;
 
