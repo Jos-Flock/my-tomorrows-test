5db911ad8753f65bdc7963ae7f4a8050df98b8f5
Jos Verhaar <jos.verhaar@flock.community>
2023-09-14 15:46:17 +0200

    feat: add unit test for study-service
    - cleaned up .ts

--- src/services/study.service.ts
+++ src/services/study.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {map, Observable } from "rxjs";
 import {Study} from "../models/study";
-import {ListResponse} from "../models/ListResponse";
+import {ListResponse} from "../models/listResponse";
 
 const API_BASE_URL: string = 'https://clinicaltrials.gov/api/v2';
 
