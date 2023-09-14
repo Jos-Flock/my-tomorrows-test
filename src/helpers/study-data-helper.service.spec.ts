69de8ed1b5813c45fcd8a571e8e19bac74779d46
Jos Verhaar <jos.verhaar@flock.community>
2023-09-14 15:45:52 +0200

    feat: add unit test for study-data-helper
    - cleaned up .ts

--- src/helpers/study-data-helper.service.spec.ts
+++ src/helpers/study-data-helper.service.spec.ts
@@ -1,16 +1,97 @@
-import { TestBed } from '@angular/core/testing';
-
-import { StudyDataHelperService } from './study-data-helper.service';
+import {DEFAULT_STUDY_LIMIT, StudyDataHelperService} from './study-data-helper.service';
+import {StudyService} from "../services/study.service";
+import {of} from "rxjs";
+import {createTestListResponse} from "../test/models/list-response";
+import {createTestStudy} from "../test/models/study-test";
 
 describe('StudyDataHelperService', () => {
   let service: StudyDataHelperService;
+  let studyService: jest.Mocked<StudyService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(StudyDataHelperService);
+    studyService = {
+      list: jest.fn().mockReturnValue(of(createTestListResponse([createTestStudy(), createTestStudy()])))
+    } as any;
+
+    service = new StudyDataHelperService(studyService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('fetchData', () => {
+    it('should pass on the default pageLimit', () => {
+      service.init(DEFAULT_STUDY_LIMIT);
+      expect(studyService.list).toHaveBeenCalledWith(DEFAULT_STUDY_LIMIT, undefined);
+    });
+
+    it('should pass on the given pageLimit', () => {
+      service.init(20);
+      expect(studyService.list).toHaveBeenCalledWith(20, undefined);
+    });
+  });
+
+  describe('addStudyToStudiesList', () => {
+    it('should keep studies count === studyLimit', (done) => {
+      studyService.list.mockReturnValue(of(createTestListResponse([createTestStudy(), createTestStudy('nieuwe'), createTestStudy()])));
+      service.init(2);
+
+      service.getCurrentStudiesObservable().subscribe(result => {
+        expect(result.length).toBe(2);
+        done();
+      });
+    });
+
+    it('should remove oldest study', (done) => {
+      studyService.list.mockReturnValue(of(createTestListResponse([
+        createTestStudy('new'), createTestStudy('newer'), createTestStudy('newest')
+      ])));
+      service.init(2);
+
+      service.getCurrentStudiesObservable().subscribe(result => {
+        expect(result[0].nctId).toBe('newer');
+        expect(result[1].nctId).toBe('newest');
+        done();
+      });
+    });
+  });
+
+  describe('startPollingTimer', () => {
+    let msToRunTimer: number = 200;
+    beforeEach(() => {
+      jest.useFakeTimers(); // Turn on fake timers
+      // Init the service with a shorter interval for speeding up unit testing and start the pollingTimer
+      service.init(DEFAULT_STUDY_LIMIT, msToRunTimer);
+      service.togglePolling();
+    });
+
+    it('should call studyService 3 times', () => {
+      jest.advanceTimersByTime(msToRunTimer + 5); // make sure the timer is triggered once
+      service.togglePolling(); // turn of the pollingTimer
+      expect(studyService.list).toHaveBeenCalledTimes(3); // 1 for the service.init + 2 times for the pollingTimer
+    });
+
+    it('should call studyService twice if timer is stopped before first completion', () => {
+      jest.advanceTimersByTime(msToRunTimer * 0.5); // make sure the timer is triggered once
+      service.togglePolling(); // turn of the pollingTimer
+      expect(studyService.list).toHaveBeenCalledTimes(2); // 1 for the service.init + 2 times for the pollingTimer
+    });
+
+    // TODO: Check of einde pagina er is
+
+  });
+
+  describe('togglePolling', () => {
+    it('should toggle isRunning', (done) => {
+      service.init(DEFAULT_STUDY_LIMIT);
+      service.togglePolling();
+      service.getPollingStatusObservable().subscribe(result => {
+        expect(result).toBe(true);
+        done();
+      });
+    });
+
+  });
+
 });
--- src/helpers/study-data-helper.service.ts
+++ src/helpers/study-data-helper.service.ts
@@ -2,23 +2,16 @@ import {Injectable, OnDestroy} from '@angular/core';
 import {StudyService} from "../services/study.service";
 import {Study} from "../models/study";
 import {
-  BehaviorSubject, concat,
-  concatMap,
-  filter,
-  finalize,
-  interval,
-  map,
+  BehaviorSubject,
   Observable,
-  of, repeat,
-  Subject, switchMap,
+  Subscription,
   takeWhile,
-  tap,
   timer
 } from "rxjs";
 import {cloneDeep} from "lodash";
 
 const DEFAULT_POLLING_INTERVAL_MS: number = 5000; // 5 seconds
-const DEFAULT_PROGRESS_INTERVAL_MS: number = 1000; // 1 second
+export const DEFAULT_STUDY_LIMIT: number = 10;
 
 @Injectable({
   providedIn: 'root'
@@ -33,13 +26,9 @@ export class StudyDataHelperService implements OnDestroy {
   private settings: {
     studyLimit: number,
     pollingInterval: number,
-    value: number,
     nextPageToken?: string,
-  } = { studyLimit: 10, pollingInterval: DEFAULT_POLLING_INTERVAL_MS, value: 0 };
-
-  public currentStudies$: Observable<Study[]> = this.studies$.asObservable();
-  public currentPollingStatus$: Observable<boolean> = this.pollingStatus$.asObservable();
-  public currentProgressPercentage$: Observable<number> = this.progressPercentage$.asObservable();
+  } = { studyLimit: DEFAULT_STUDY_LIMIT, pollingInterval: DEFAULT_POLLING_INTERVAL_MS};
+  private pollingTimer!: Subscription;
 
   constructor(private readonly studyService: StudyService) {
   }
@@ -54,7 +43,6 @@ export class StudyDataHelperService implements OnDestroy {
   }
 
   private addStudyToStudiesList(studies: Study[]): void {
-    console.log('addStudyToStudiesList START -> this.studiesList:', this.studiesList);
     // can hold max this.settings.studyLimit (10)
     // set the current studiesList as starting point
     const newStudiesList: Study[] = this.studiesList;
@@ -70,36 +58,15 @@ export class StudyDataHelperService implements OnDestroy {
     // copy the newStudiesList into the studiesList
     this.studiesList = newStudiesList;
 
-    console.log('addStudyToStudiesList END -> this.studiesList:', cloneDeep(this.studiesList));
     this.studies$.next(newStudiesList);
   }
 
   private startPollingTimer(): void {
-    console.log('startTimer');
-    timer(0, this.settings.pollingInterval).pipe(
+    // TODO: Uitzoeken of de timer() zelf destroyed?
+    this.pollingTimer = timer(0, this.settings.pollingInterval).pipe(
       takeWhile(() => this.isRunning)
     ).subscribe(() => {
-      console.log('polling');
-      console.log('this.settings.nextPageToken:', this.settings.nextPageToken);
       this.fetchData(1);
-    });
-  }
-
-  private startProgressTimer(): void {
-    const pollingIntervalInSeconds = this.settings.pollingInterval / 1000;
-    console.log('start progress timer');
-    timer(0, DEFAULT_PROGRESS_INTERVAL_MS).pipe(
-      takeWhile(() => this.isRunning),
-      tap(() => this.settings.value = this.settings.value + 1),
-      tap(() => {
-        if (this.settings.value > pollingIntervalInSeconds) {
-          this.settings.value = 0
-        }
-      })
-    ).subscribe(() => {
-      console.log('this.settings.value:', this.settings.value);
-      console.log('this.settings.value in percent:', this.settings.value * (100 / (this.settings.pollingInterval / 1000)));
-      this.progressPercentage$.next(this.settings.value * (100 / pollingIntervalInSeconds));
     });
   }
 
@@ -115,73 +82,22 @@ export class StudyDataHelperService implements OnDestroy {
 
     if (this.isRunning) {
       this.startPollingTimer();
-      // this.startProgressTimer();
-      // this.startPollingTimerTest();
     }
   }
 
-  /*******************************/
-
-  // will emit whether or not data is currently loading.
-  public dataLoadingSubject$ = new Subject<boolean>();
-
-  // wrapper for the actual http reqeust that invokes datLoadingSubject before and after.
-  private doRequest(): Observable<any> {
-    // arbitrary value to start the pipe.
-    return of(0).pipe(
-      // notify load start
-      tap(() => this.dataLoadingSubject$.next(true)),
-      // wait for actual request.
-      concatMap(() =>  of(this.fetchData(1))),
-      // notify load end
-      finalize(() => this.dataLoadingSubject$.next(false))
-    )
+  public getCurrentStudiesObservable(): Observable<Study[]> {
+    return this.studies$.asObservable();
   }
 
-  // will emit the countdown, 5, 4, 3, ...
-  public dataRequestTimeRemaining$ = new Subject<number>();
-
-  // Creates an Observable that will count from interval to 0, then complete.
-  private countDown(interval: number): Observable<number> {
-    // timer() will emit an incrementing number each second
-    return timer(0, 1000).pipe(
-      takeWhile(() => this.isRunning),
-      // i is emitted by timer(), basically the number of elapsed seconds.
-      takeWhile((i) => i <= interval),
-      // trigger output in other Observable
-      tap((i) => this.dataRequestTimeRemaining$.next(interval - i)),
-      // suppress any emission, we don't want them in the main pipe.
-      filter(() => false)
-    )
+  public getPollingStatusObservable(): Observable<boolean> {
+    return this.pollingStatus$.asObservable();
   }
-
-  // wil emit if the user selects a different interval.
-  public interval$ = new BehaviorSubject<number>(5);
-
-  // Output is the loaded data.
-  private data$ = this.interval$.pipe(
-    switchMap((interval) => concat( // restart all, if interval changes
-      this.countDown(interval),                     // first run countDown
-      this.doRequest()                              // when countDown completes, start the request
-      ).pipe(repeat({ delay: 0}))      // repeat countDown + request when finished
-    )
-  );
-
-  private startPollingTimerTest(): void {
-    // subscribe to everything
-    // instead of logging you'd probably use an async-pipe
-    this.dataLoadingSubject$.subscribe((loading) => console.log(loading ? 'loading' : 'not loading'));
-    this.dataRequestTimeRemaining$.subscribe((t) => console.log(`time until request: ${t}s...`));
-    this.data$.subscribe(console.log);
-  }
-
-
-  /*******************************/
 
   ngOnDestroy() {
     this.studies$.complete();
     this.pollingStatus$.complete();
     this.progressPercentage$.complete();
+    this.pollingTimer.unsubscribe();
   }
 
 }
