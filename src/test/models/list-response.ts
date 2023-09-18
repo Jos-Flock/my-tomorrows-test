fecc7be324eb564bb87ead3a4d4d27d9e6e24c5f
Jos Verhaar <jos.verhaar@flock.community>
2023-09-18 15:01:53 +0200

    refactor: change passing params for optional parameters
    Signed-off-by: Jos Verhaar <jos.verhaar@flock.community>

--- src/test/models/list-response.ts
+++ src/test/models/list-response.ts
@@ -1,5 +1,5 @@
 import { ListResponse } from '../../models/listResponse';
 
-export function createTestListResponse<T>(items: T[]): ListResponse<T> {
-  return new ListResponse<T>('nextPageToken', items);
+export function createTestListResponse<T>(items: T[], nextPageToken: string | null = 'nextPageToken'): ListResponse<T> {
+  return new ListResponse<T>(items, nextPageToken);
 }
